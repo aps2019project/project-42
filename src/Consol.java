8d0c0d9602a22a02c20be06e0c2daa715b643ce6
Danyal0096 <farahany.danyal@gmail.com>
2019-04-29 08:58:32 +0430

    mvc started

---
+++ src/Consol.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Console {
+    Scanner scanner= new Scanner(System.in);
+    Console(Scanner scanner){
+        String command= scanner.nextLine();
+        if() {
+
+        }
+        else if () {
+
+        }
+    }
+}
