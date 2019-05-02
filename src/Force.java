ce78811fd0acd5dc0de29d27bb54f0344abf2817
Danyal0096 <farahany.danyal@gmail.com>
2019-05-02 22:04:31 +0430

    battle 42 percent

---
+++ src/Force.java
@@ -0,0 +1,10 @@
+public class Force extends SpellCard {
+    int AP;//Attack Power
+    int HP;//Health Power
+    RangeType rangeType;
+    int range;
+    //push sho dg :///
+    public Force(String name) {
+        super(name);
+    }
+}
---
+++ src/RangeType.java
@@ -0,0 +1,5 @@
+public enum RangeType {
+    melee,
+    ranged,
+    hybrid
+}
