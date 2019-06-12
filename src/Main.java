9e027b607fecfe7b5397d0f8aeb49736bc705d41
dorna <dornadehghani@ce.sharif.edu>
2019-06-12 12:52:38 +0430

    move to javafx/menu graphic started

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
     static Scanner scanner = new Scanner(System.in);
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(graphic.Sample.class.getResource("sample.fxml"));
+        primaryStage.setTitle("Duelyst");
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) throws IOException {
-        Duelyst duelyst = new Duelyst();
-        duelyst.main();
+        launch(args);
+        /*Duelyst duelyst = new Duelyst();
+        duelyst.main();*/
     }
 }
